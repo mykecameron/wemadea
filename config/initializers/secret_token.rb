70d99339ca82de6d741ded266536585c3641035c
Myke Cameron <mykecameron@gmail.com>
2014-01-31 18:43:10 -0500

    generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wemadea::Application.config.secret_key_base = '2f1085ead51a5d5591eaeb19d48a500166ec1ea043bbc8a07a600eafbb5f473d0f914df845b4995da458a416ff6a6c5b5b9f97b3061dc7a1c5c84a25be6cc6ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wemadea::Application.config.session_store :cookie_store, key: '_wemadea_session'
