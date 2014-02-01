18899d94656bb45ef841ff19bb74216c694b9acf
Myke Cameron <mykecameron@gmail.com>
2014-02-01 11:30:55 -0500

    add main layout and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Wemadea::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root 'pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
