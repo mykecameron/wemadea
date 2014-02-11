091cb974c46d5016cd1ca881fa03a728ce509fc9
Myke Cameron <mykecameron@gmail.com>
2014-02-10 20:35:58 -0500

    resize images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ActiveRecord::Base
-  has_attached_file :image_file
+  has_attached_file :image_file, styles: { background: '950x655#' }
   validates_attachment_content_type :image_file, :content_type => /\Aimage\/.*\Z/
 end
