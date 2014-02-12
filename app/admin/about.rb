c64121131a3d8ccf783ae9377ca8ea90d82d0464
Myke Cameron <mykecameron@gmail.com>
2014-02-11 19:23:49 -0500

    fix indentation

--- app/admin/about.rb
+++ app/admin/about.rb
@@ -2,10 +2,10 @@ ActiveAdmin.register About do
   menu label: "About"
   permit_params :content
 
-   form do |f|
-     f.inputs do
-       f.input :content, label: 'Content (format with HTML)'
-       f.actions
-     end
-   end
+  form do |f|
+    f.inputs do
+      f.input :content, label: 'Content (format with HTML)'
+      f.actions
+    end
+  end
 end
