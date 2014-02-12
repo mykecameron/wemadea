ea3f64143c3dfb9932e54cb1de81a30154f98d11
Myke Cameron <mykecameron@gmail.com>
2014-02-11 20:12:10 -0500

    prevent saving multiple thanks or abouts

--- app/models/about.rb
+++ app/models/about.rb
@@ -1,3 +1,11 @@
 class About < ActiveRecord::Base
   validates :content, presence: true
+  validate :there_can_be_only_one
+
+  def there_can_be_only_one
+    return true if Thank.count == 0
+    unless persisted?
+      errors.add(:content, "There can be only one about section. Please edit the existing About.")
+    end
+  end
 end
--- app/models/thank.rb
+++ app/models/thank.rb
@@ -1,2 +1,11 @@
 class Thank < ActiveRecord::Base
+  validates :content, presence: true
+  validate :there_can_be_only_one
+
+  def there_can_be_only_one
+    return true if Thank.count == 0
+    unless persisted?
+      errors.add(:content, "There can be only one thanks section. Please edit the existing Thanks.")
+    end
+  end
 end
